eeb0788111f8295cf9bd7bb292b34b11bd863630
Marvis Alimhiantare <marvis.alimhiantare@gmail.com>
2024-07-05 11:10:55 +0100

    task-route refactored

--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { getTasks, getTask, postTask, putTask, deleteTask } = require("../controllers/taskController.js");
+const { getTasks, postTask, getTask, putTask, deleteTask } = require("../controllers/taskController.js");
 const { verifyAccessToken } = require("../middleware");
 
 // Routes beginning with /api/tasks
-router.get("/verify", verifyAccessToken, getTasks);
-router.get("/:taskId", verifyAccessToken, getTask);
+router.get("/", verifyAccessToken, getTasks);
 router.post("/addTask", verifyAccessToken, postTask);
+router.get("/:taskId", verifyAccessToken, getTask);
 router.put("/:taskId", verifyAccessToken, putTask);
 router.delete("/:taskId", verifyAccessToken, deleteTask);
 
