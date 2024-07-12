06ee5794660dbe6a6955f090ffb9470f819b752c
Marvis Alimhiantare <marvis.alimhiantare@gmail.com>
2024-07-12 11:11:16 +0100

    app.js file modified

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ app.use("/api/task", taskRoute);
 app.use("/api/profile", profileRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "../Task_Manager_Frontend/build")));
-  app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "../Task_Manager_Frontend/build/index.html")));
+  app.use(express.static(path.resolve(__dirname, "../Frontend/build")));
+  app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "../build/index.html")));
 }
 
 const port = process.env.PORT || 5000;
